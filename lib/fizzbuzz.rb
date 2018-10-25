5e9716b81ec6d5d42cf9ab80e52ac539ecda7876
Ajay <ajaymungur@hotmail.com>
2018-10-25 12:27:53 +0000

    passes fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,26 @@
 def fizzbuzz(num)
-  if num == 3
-    "fizz"
-  elsif num == 5
+  if num % 3 == 0 && num % 5 == 0
+    "fizzbuzz"
+  elsif num % 5 == 0
     "buzz"
+  elsif num % 3 == 0
+    "fizz"
   else
     num
   end
 end
+
+# Alternative:
+# def fizzbuzz(num)
+#   if num % 3 == 0
+#     if num % 5 == 0
+#       "fizzbuzz"
+#     else
+#       "fizz"
+#     end
+#   elsif num % 5 == 0
+#     "buzz"
+#   else
+#     num
+#   end
+# end
