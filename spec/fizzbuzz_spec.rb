1c72bcf3a43f045ec34089b4675cc63bee76c27b
Ajay <ajaymungur@hotmail.com>
2018-10-25 12:13:31 +0000

    fizz, buzz and num tested

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,3 +4,15 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3)).to eq "fizz"
   end
 end
+
+describe 'fizzbuzz' do
+  it 'gives 5 expects "buzz"' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'gives 7 expects 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
+end
