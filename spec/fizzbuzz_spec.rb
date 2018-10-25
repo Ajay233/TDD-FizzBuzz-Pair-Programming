e9fa4a0d7dce0937ae90edc9f7d1bd9232b57519
Ajay <ajaymungur@hotmail.com>
2018-10-25 12:50:21 +0000

    added extra test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -22,3 +22,28 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
 end
+
+describe 'fizzbuzz' do
+  it 'gives 6 expects "fizz"' do
+    expect(fizzbuzz(6)).to eq "fizz"
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'gives 10 expects "buzz' do
+    expect(fizzbuzz(10)).to eq "buzz"  
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'gives 11 expects 11' do
+    expect(fizzbuzz(11)).to eq 11
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'gives 30 expects 30' do
+    expect(fizzbuzz(30)).to eq "fizzbuzz"
+  end  
+end
+
